96b646f169eb2b21ffd42db086946311745d1e21
satyanathAAI <satyanath.eshwaroju@augmentedai.ai>
2024-09-05 16:29:09 +0530

    image

---
+++ lds_image/image_cache/constants.py
@@ -0,0 +1,3 @@
+# Augument_open_api_key = ''
+global_output_fodler_path = "/home/ubuntu/lds/lds_image/image_cache/assetiq-church/outputs/OUTPUT/Images"
+search_rag_api = 'http://10.10.0.212:7175/file_upload_to_search_rag_KB'
